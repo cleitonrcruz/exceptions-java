e1f6d27cec6b42cc6ac7e787ad1238f6b411e04b
Cleiton <cleiton.rcruz@gmail.com>
2025-06-25 07:58:17 -0300

    atualização2

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,12 +1,16 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
 	private Integer rommNumber;
 	private Date checkIn;
 	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Reservation(Integer rommNumber, Date checkIn, Date checkOut) {
 		this.rommNumber = rommNumber;
@@ -31,6 +35,24 @@ public class Reservation {
 		return checkOut;
 	}
 
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
 	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Room " + rommNumber + 
+				", check-in: " + sdf.format(checkIn) + 
+				", check-out: " + sdf.format(checkOut) + 
+				", " +
+				duration() +
+				" nights.";
+	}
 	
 }
